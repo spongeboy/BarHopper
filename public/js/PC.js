8312ac6e859b6916ed5ee4e2dc75e6808c229625
pat macpherson <pat@macpherson.id.au>
2012-07-20 12:56:41 +1000

    Looking good!

--- public/js/PC.js
+++ public/js/PC.js
@@ -43,60 +43,22 @@ PC.core = (function(core, $, undefined){
 				id: 0,
 				name : 'The Australian Heritage Hotel',
 				address : '100 Cumberland Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859583,151.207038),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
+				latlng : new google.maps.LatLng(-33.859583,151.207038)
 			},
 			{
 				id: 1,
 				name : 'The Glenmore Hotel',
 				address : '96 Cumberland Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.858778,151.207294),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
+				latlng : new google.maps.LatLng(-33.858778,151.207294)
 			},
 			{
 				id: 2,
 				name : 'Lowenbrau Keller',
 				address : '18 Argyle Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
-			},
-			{
-				id: 3,
-				name : 'Fortune of War',
-				address : '137 George Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
-			},
-			{
-				id: 4,
-				name : 'Lord Nelson',
-				address : '19 Kent Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
-			},
-			{
-				id: 5,
-				name : 'Observer',
-				address : '69 George Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
-			},
-			{
-				id: 6,
-				name : 'Orient',
-				address : '89 George Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
-			},
-			{
-				id: 7,
-				name : 'Palisade',
-				address : '35 Bettington Street, The Rocks NSW, Australia',
-				latlng : new google.maps.LatLng(-33.859079,151.207838),
-				comments: 'Este bar es una mierda, pero me dejan beber hasta que pierdo el sentido de la verticalidad'
+				latlng : new google.maps.LatLng(-33.859079,151.207838)
 			}
-
 		];*/
+
 		core.debug = function(){console.log(pubCrawl)};
 		mediator.on('mapInitialised', function(){
 			 PC.wizard.init();
@@ -106,7 +68,7 @@ PC.core = (function(core, $, undefined){
 			newPub = true;
 			if(pubs[id]){
 				for(var i=0, l = pubCrawl.length; i<l; i++){
-					if(pubCrawl[i].id === id) newPub = false;  
+					if(pubCrawl[i].id === id) newPub = false;
 				}
 				if (newPub) pubCrawl.push(pubs[id]);
 			}
@@ -117,7 +79,7 @@ PC.core = (function(core, $, undefined){
 		mediator.on('deletePub', function(data){
 		console.log(pubCrawl);
 			for(var i=0, l = pubCrawl.length; i<l; i++){
-				if(pubCrawl[i].id === data[0]) pubCrawl.splice(i,1) 
+				if(pubCrawl[i].id === data[0]) pubCrawl.splice(i,1)
 			}
 			PC.mapManager.manageMarkers({control: 'remove', id: data[0]})
 			PC.mapManager.route({pubCrawl:pubCrawl, travelMode : 'WALKING'});
@@ -126,7 +88,7 @@ PC.core = (function(core, $, undefined){
 		mediator.on('modifyPubCrawl', function(data){
 			pubCrawl = [];
 			var list = data[0];
-			for(var i=0, l=list.length; i<l; i++){ 
+			for(var i=0, l=list.length; i<l; i++){
 				pubCrawl.push(pubs[list[i]]);
 			}
 			PC.mapManager.route({pubCrawl:pubCrawl, travelMode : 'WALKING'});
@@ -162,25 +124,25 @@ PC.core = (function(core, $, undefined){
 					preserveViewport : true
 				}
 			}
-			
+
 			PC.mapManager.init({mode:'wizard',map : map, directions : directions, pubs : pubs});
 		}
 	};
-	
+
 	var init = function(){
 		var mapping = {
 			wizard : wizardManager
 		};
-	
+
 		$(document).ready(function(){
 			section = $('body').data('section');
 			step = $('body').data('step');
-			
+
 			mapping[section]();
-			
+
 		});
 	}();
-	
+
 	return core;
 
 })(PC.core || {}, jQuery);
@@ -193,13 +155,22 @@ PC.mapManager = (function(mapManager, $, undefined){
 	geocoder,
 	markers = []
 	;
-	
+
 	var loadBars = function(pubs){
+
 		var getLocationFromAddress = function(i){
-			geocoder.geocode( { 'address': pubs[i].address}, function(results, status) {
+
+		/*	geocoder.geocode( { 'address': pubs[i].address}, function(results, status) {
 				if (status == google.maps.GeocoderStatus.OK) {
-					 pubs[i].latlng = results[0].geometry.location; 
-				
+				//	 pubs[i].latlng = results[0].geometry.location;
+				} else {
+					console.log("Geocode was not successful for the following reason: " + status);
+				}
+			});*/
+		};
+		//for (var i=0, l = pubs.length; i<l; i++){
+        for (id in pubs){
+			getLocationFromAddress(id);
 			var marker = new google.maps.Marker({
 				position: pubs[id].latlng,
 				map: map,
@@ -220,19 +191,12 @@ PC.mapManager = (function(mapManager, $, undefined){
 			});
 			google.maps.event.addListener(marker, 'mouseout', function() {
 				this.setIcon('resources/images/pub_marker_unselected.png');
-			});			
-				} else {
-					console.log("Geocode was not successful for the following reason: " + status);
-				}
 			});
-		};
-		for (var i=0, l = pubs.length; i<l; i++){
-			getLocationFromAddress(i);	
 		}
 	};
-	
+
 	mapManager.manageMarkers = function(config){
-	
+
 		var baseImageUrl = 'resources/images/pub_marker_',
 		marker = markers[config.id];
 		if(config.control === 'select') baseImageUrl += 'selected';
@@ -243,75 +207,75 @@ PC.mapManager = (function(mapManager, $, undefined){
 		});
 		google.maps.event.addListener(marker, 'mouseout', function() {
 			this.setIcon(baseImageUrl+'.png');
-		});	
+		});
 	};
-	
+
 	mapManager.init = function(config){
 		map = new google.maps.Map(config.map.element, config.map.options);
 		directionsDisplay = new google.maps.DirectionsRenderer(config.directions.options);
 		directionsService = new google.maps.DirectionsService();
 		geocoder = new google.maps.Geocoder();
 		directionsDisplay.setMap(map);
-		
+
 	/*	var $wizardSearchBar = $('<div id="wizardSearch"><input type="text" value="Search" name="wizardSearchBar" id="wizardSearchBar"/></div>');
 		map.controls[google.maps.ControlPosition.TOP_LEFT].push($wizardSearchBar[0]);*/
 
 		if (config.pubs.length > 0){
 			loadBars(config.pubs)
 		} else console.log('Pubs object is empty');
-		
+
 		google.maps.event.addListener(map, 'click', function(e) {
 			// Handle adding a new venue
 		});
-		
+
 		/*
 			Handle click in path
 			Handle drag in path
 		*/
-		
+
 		if (mediator){ mediator.publish('mapInitialised');}
 		else console.log('mediator is missing');
 	};
-	
+
 	mapManager.route = function(config){
-		
+
 		var request = {
 			travelMode :  google.maps.TravelMode[config.travelMode]
 		};
-		
-		if(config.pubCrawl.length > 1){ 
-			request.origin = config.pubCrawl[0].address;
-			request.destination = config.pubCrawl[config.pubCrawl.length-1].address;
+
+		if(config.pubCrawl.length > 1){
+			request.origin = config.pubCrawl[0].latlng;
+			request.destination = config.pubCrawl[config.pubCrawl.length-1].latlng;
 			request.waypoints = [];
-			for (var i=1, l = config.pubCrawl.length-1; i<l; i++) { 
+			for (var i=1, l = config.pubCrawl.length-1; i<l; i++) {
 				request.waypoints.push({
-					location: config.pubCrawl[i].address,
+					location: config.pubCrawl[i].latlng,
 					stopover: true
 				});
 			}
 			directionsService.route(request, function(response, status) {
 				if (status == google.maps.DirectionsStatus.OK) {
-					directionsDisplay.setMap(map);	
+					directionsDisplay.setMap(map);
 					directionsDisplay.setDirections(response);
 				  // Handle distances
 				}
 			});
 		} else {
-			directionsDisplay.setMap(null);		
+			directionsDisplay.setMap(null);
 		}
 	};
-	
+
 	return mapManager;
 
 })(PC.mapManager || {}, jQuery);
 
 
 PC.wizard = (function(wizard, $, undefined){
-	
+
 	var $timeline,
 	$sidePanel
 	;
-	
+
 	var controlSidePanel = function(control){
 		var mode = {
 			toggle : function(){
@@ -327,13 +291,13 @@ PC.wizard = (function(wizard, $, undefined){
 		};
 		mode[control]();
 	};
-	
+
 	wizard.init = function(){
 		$timeline = $('#timeline');
 		$sidePanel = $('#sidePanel');
 		var $timeList = $timeline.find('ul');
 		$timeList.sortable();
-		
+
 		$('a.closePanel').click(function(e){
 			e.preventDefault();
 			$sidePanel.data('id','');
@@ -346,12 +310,13 @@ PC.wizard = (function(wizard, $, undefined){
 			else console.log('mediator is missing');
 		});
 		$(document).on("click","a.deletePub", function(e){
-			e.preventDefault(); 
+			e.preventDefault();
 			if (mediator) mediator.publish('deletePub',$(this).parent().data('id'));
 			else console.log('mediator is missing');
 		});
 		$(document).on("click","span.pubName", function(e){
 			e.preventDefault();
+            console.log($(this).parent().data('id'));
 			if (mediator) mediator.publish('displayPubDetails',$(this).parent().data('id'));
 			else console.log('mediator is missing');
 		});
@@ -363,20 +328,20 @@ PC.wizard = (function(wizard, $, undefined){
 			if (mediator){ mediator.publish('modifyPubCrawl', idList);}
 			else console.log('mediator is missing');
 		});
-		
+
 		if (mediator){ mediator.publish('wizardInitialised');}
 		else console.log('mediator is missing');
 	};
-	
+
 	wizard.displayPubDetails = function(pub){
-		$sidePanel.find('#pubName').text(pub.name).end().find('#pubAddress').text(pub.address).end().find('#comments').text(pub.comments);
+        $sidePanel.find('#pubName').text(pub.name).end().find('#pubAddress').text(pub.address);
         $sidePanel.data('id',pub.id);
         controlSidePanel('open');
 	};
-	
+
 	wizard.updateTimeline = function(pubCrawl){
 		var	$target = $timeline.find('ul').html(''),
-		$item = $('<li><a href="" class="deletePub"></a><span class="pubName"></span></li>'),
+		$item = $('<li><a href="" class="deletePub">X</a><span class="pubName"></span></li>'),
 		aux
 		;
 		for(var i=0, l=pubCrawl.length; i<l; i++){ 
